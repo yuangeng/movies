79c08e536b68e00cde177e4b6a0850fd4a945a7c
vagrant <vagrant@kellogg-dev-box>
2014-04-21 05:11:13 +0000

    Adding movies to the webpage

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140421044046) do
+ActiveRecord::Schema.define(version: 20140421051055) do
 
   create_table "movies", force: true do |t|
     t.string  "title"
